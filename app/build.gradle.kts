26475b40429d71410dd817874cdb0b3a4949b9ce
bw-green <bwgreen621@gmail.com>
2024-11-29 01:40:03 +0900

    버전다시 바꾸기

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,12 +13,12 @@ properties.load(propertiesFile.inputStream())
 
 android {
     namespace = "com.example.kuit4_android_retrofit"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.kuit4_android_retrofit"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -68,12 +68,11 @@ dependencies {
     kapt("androidx.room:room-compiler:$room_version")
     implementation("androidx.room:room-ktx:$room_version")
 
-    val retrofit_version = "2.9.0"
+    val retrofit_version = "2.6.1"
 // Retrofit 라이브러리
     implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
 // Gson Converter 라이브러리
     implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
 // Scalars Converter 라이브러리
     implementation("com.squareup.retrofit2:converter-scalars:$retrofit_version")
-
 }
